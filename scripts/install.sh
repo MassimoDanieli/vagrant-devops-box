04449fbcc85b98dafc8dda512a1d1662715e8dd0
MaxD70 <87365596+MaxD70@users.noreply.github.com>
2021-07-14 16:27:44 +0100

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# remove comment if you want to enable debugging
+#set -x
+
+if [ -e /etc/redhat-release ] ; then
+  REDHAT_BASED=true
+fi
+
+TERRAFORM_VERSION="0.15.5"
+PACKER_VERSION="1.7.3"
+
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+if [ ${REDHAT_BASED} ] ; then
+  yum -y update
+  yum install -y docker ansible unzip wget
+else
+  apt-get update
+  apt-get -y install docker.io ansible unzip python3-pip curl
+fi
+# add docker privileges
+usermod -G docker ubuntu
+usermod -G docker vagrant
+# install awscli and ebcli
+pip3 install -U awscli
+pip3 install -U awsebcli
+
+#terraform
+
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+
+wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# skaffold
+
+curl -s -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/ \
+&& rm skaffold \
+&& echo "Skaffold installed"
+# kubectl
+
+wget -q "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+&& sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl \
+&& rm kubectl \
+&& echo "Kubectl installed"
+
+# clean up
+if [ ! ${REDHAT_BASED} ] ; then
+  apt-get clean
+fi
+
+echo "DevOps box ready to use"
